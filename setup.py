9af7f70fd5269668e80cf591e51b4cf5e2b27782
Chris Glass <christopher.glass@divio.ch>
2011-05-05 10:37:50 +0200

    Packaging modifications

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Christopher Glass",
     author_email="tribaal@gmail.com",
     name='django-shop-simplevariations',
-    version='0.0.0',
+    version='0.0.1',
     description='A simple to use product variations module for django SHOP',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='http://www.django-shop.org/',
