a76dd5da3aaab4da2b6acfea29e44fa8d7d15e3c
Martin Brochhaus <mbrochh@gmail.com>
2011-06-08 15:57:10 +0800

    enhanced OptionGroup admin with FilteredSelectMultiple widget and prepopulated slug field /  added build/ to .gitignore

--- shop_simplevariations/admin.py
+++ shop_simplevariations/admin.py
@@ -1,6 +1,9 @@
 #-*- coding: utf-8 -*-
+from django.db import models
 from django.contrib import admin
 from django.contrib.admin.options import TabularInline, ModelAdmin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.utils.translation import ugettext_lazy as _
 from shop_simplevariations.models import Option, OptionGroup
 
 class OptionInline(TabularInline):
@@ -8,5 +11,12 @@ class OptionInline(TabularInline):
 
 class OptionGroupAdmin(ModelAdmin):
     inlines = [OptionInline,]
+    prepopulated_fields = {"slug": ("name",)}
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple(
+            verbose_name=_('products'),
+            is_stacked=False
+            )},
+    }
 
 admin.site.register(OptionGroup, OptionGroupAdmin)
