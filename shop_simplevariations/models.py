ba2aca5b21d85f7f5238d6c0518f28b1efb09cc2
Martin Brochhaus <mbrochh@gmail.com>
2011-06-14 10:56:48 +0800

    Implementing more elaborate code that otherwise would have to be done by users of simplevariations anyways (I think)

--- shop_simplevariations/models.py
+++ shop_simplevariations/models.py
@@ -8,18 +8,18 @@ class OptionGroup(models.Model):
     '''
     A logical group of options
     Example:
-    
+
     "Colors"
     '''
     name = models.CharField(max_length=255)
     slug = models.SlugField() # Used in forms for example
     description = models.CharField(max_length=255, blank=True, null=True)
-    products = models.ManyToManyField(Product, related_name="option_groups", 
+    products = models.ManyToManyField(Product, related_name="option_groups",
                                       blank=True, null=True)
-    
+
     def __unicode__(self):
         return self.name
-        
+
     def get_options(self):
         '''
         A helper method to retrieve a list of options in this OptionGroup
@@ -30,7 +30,7 @@ class OptionGroup(models.Model):
 class Option(models.Model):
     '''
     A product option. Examples:
-    
+
     "Red": 10$
     "Blue": 5$
     ...
@@ -38,18 +38,17 @@ class Option(models.Model):
     name = models.CharField(max_length=255)
     price = CurrencyField() # Can be negative
     group = models.ForeignKey(OptionGroup)
-        
+
     def __unicode__(self):
         return self.name
-    
+
 class CartItemOption(models.Model):
     '''
     This holds the relation to product options from the cart item.
     It allows to know which options where selected for what cartItem.
-    
-    Generally, this is used by 
+
+    Generally, this is used by
     shop.cart.modifiers.product_options.ProductOptionsModifier
     '''
     cartitem = models.ForeignKey(CartItem)
     option = models.ForeignKey(Option)
-    
---
+++ shop_simplevariations/templatetags/simplevariation_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+from ..models import CartItemOption
+
+register = template.Library()
+
+
+@register.filter
+def get_option_groups(value):
+    """Returns all option groups for the given product."""
+    return value.option_groups.all()
+
+
+@register.filter
+def get_options(value):
+    """Returns all options for the given option group."""
+    return value.option_set.all()
