ac5372bf34f73489e7d5ddef9f1478c13225865f
Chris Glass <christopher.glass@divio.ch>
2011-07-13 14:00:54 +0200

    Adapted cart modifiers to API in latest django-shop version (0.0.9)

--- shop_simplevariations/cart_modifier.py
+++ shop_simplevariations/cart_modifier.py
@@ -7,28 +7,34 @@ class ProductOptionsModifier(BaseCartModifier):
     This modifier adds an extra field to the cart to let the lineitem "know"
     about product options and their respective price.
     '''
-    def add_extra_cart_item_price_field(self, cart_item):
+    def process_cart_item(self, cart_item):
         '''
-        This adds a list of price modifiers dependeing on the product options
+        This adds a list of price modifiers depending on the product options
         the client selected for the current cart_item (if any)
         '''
         selected_options = CartItemOption.objects.filter(cartitem=cart_item)
-
         for selected_opt in selected_options:
             option_obj = selected_opt.option
-            data = (option_obj.name, option_obj.price * cart_item.quantity)
+            price = option_obj.price * cart_item.quantity
+            data = (option_obj.name, price)
+            # Don't forget to update the running total!
+            cart_item.current_total = cart_item.current_total + price
             cart_item.extra_price_fields.append(data)
-
         return cart_item
     
     
 class TextOptionsModifier(BaseCartModifier):
     """
-    THis price modifier appends all the text options it finds in the database for
+    This price modifier appends all the text options it finds in the database for
     a given cart item to the item's extra_price_fields.
     """
-    def add_extra_cart_item_price_field(self, cart_item):
+    def process_cart_item(self, cart_item):
         text_options = CartItemTextOption.objects.filter(cartitem=cart_item)
         for text_opt in text_options:
-            data = ('Text: "%s"', text_opt.text_option.price)
+            price = text_opt.text_option.price
+            data = ('Text: "%s"', price)
+            # Don't forget to update the running total!
+            cart_item.current_total = cart_item.current_total + price
+            #Append to the cart_item's list now.
             cart_item.extra_price_fields.append(data)
+        return cart_item
