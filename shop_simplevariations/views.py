4364b5023d205078fc291c8a092de5a521fec557
Chris Glass <christopher.glass@divio.ch>
2011-07-13 10:48:44 +0200

    Changed the custom cartdetails view to handle TextOptions as well as Options

--- shop_simplevariations/views.py
+++ shop_simplevariations/views.py
@@ -1,11 +1,12 @@
+from .models import Option, CartItemOption
 from django.db.models import Q
-
 from shop.models.cartmodel import CartItem
 from shop.models.productmodel import Product
 from shop.util.cart import get_or_create_cart
 from shop.views.cart import CartDetails
+from shop_simplevariations.models import TextOption, CartItemTextOption
 
-from .models import Option, CartItemOption
+
 
 
 class SimplevariationCartDetails(CartDetails):
@@ -22,9 +23,13 @@ class SimplevariationCartDetails(CartDetails):
 
         #now we need to find out which options have been chosen by the user
         option_ids = []
+        text_option_ids = {} # A dict of {TextOption.id:CartItemTextOption.text}
         for key in self.request.POST.keys():
             if key.startswith('add_item_option_group_'):
                 option_ids.append(self.request.POST[key])
+            elif key.startswith('add_item_text_option_'):
+                id = key.split('add_item_text_option_')[1]
+                text_option_ids.update({id:self.request.POST[key]})
 
         #now we need to find out if there are any cart items that have the exact
         #same set of options
@@ -32,10 +37,13 @@ class SimplevariationCartDetails(CartDetails):
         found_cartitem_id = None
         merge = False
         for cartitem in qs:
-            # for each CartItem in the Cart, get it's options
+            # for each CartItem in the Cart, get it's options and text options
             cartitemoptions = CartItemOption.objects.filter(
-                cartitem=cartitem, option__in=option_ids)
-            if len(cartitemoptions) == len(option_ids):
+                cartitem=cartitem, option__in=option_ids, 
+                text_option__in=text_option_ids.keys(),
+                text_option__text__in=text_option_ids.values())
+            
+            if len(cartitemoptions) == (len(option_ids) + len(text_option_ids)):
                 found_cartitem_id = cartitem.id
                 merge = True
                 break
@@ -67,4 +75,14 @@ class SimplevariationCartDetails(CartDetails):
                 cartitem_option.cartitem = cart_item
                 cartitem_option.option = option
                 cartitem_option.save()
+            elif key.startswith('add_item_text_option_'):
+                id = key.split('add_item_text_option_')[1]
+                txt = self.request.POST[key]
+                txt_opt = TextOption.objects.get(pk=id)
+                cito = CartItemTextOption()
+                cito.text_option = txt_opt
+                cito.text = txt
+                cito.cartitem = cart_item
+                cito.save()
+                
         return self.success()
